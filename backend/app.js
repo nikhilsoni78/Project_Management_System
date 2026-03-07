a977112972f30305169bb151159b9972c01f3884
Nikhil Soni <nikhilsoni7828@gmail.com>
2026-03-07 15:37:17 +0530

    initial server created for the backend

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,17 @@
-console.log('aPP is running..')
+const express = require('express');
+const app = express();
+
+
+app.use(express.json());
+
+
+app.get('/', (req,res) => {
+    res.send('Server on hai ')
+})
+
+
+app.listen(3000, () => {
+    console.log('server is on');
+})
+
+
